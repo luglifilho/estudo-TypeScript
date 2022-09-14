b0a6a0203723698b28a31c989e55d448368a4078
luglifilho <luglifilho@hotmail.com>
2022-09-13 21:45:56 -0300

    Update a001-type-anotation.ts
    continuação aula

--- .vscode/src/a001-type/a001-type-anotation.ts
+++ .vscode/src/a001-type/a001-type-anotation.ts
@@ -1,3 +1,32 @@
-let nome: string='Daniel';
+export function multplicArgs( ...args: Array<number>): number{
+   return args.reduce((ac, valor) => ac * valor, 1);
+   }
 
-let ArrayDeNumbers: Array<number> =[1,10,15,42];
+   const result = multplicArgs(1, 2, 2)
+  console.log(result);
+
+    //tuple
+
+    const dadosCliente: [number, string, number] = [1, 'Daniel', 15];
+    const dadosCliente2: readonly [number, string, number] = [1, 'Daniel', 15];
+
+    dadosCliente[0]= 100;
+
+    console.log(dadosCliente);
+
+
+
+    export function square(x: any){
+        if(typeof x === 'number')
+        return x * x;
+        return null;
+    }
+
+    const squareof = square('ola');
+
+
+    let a: 100 = 100;
+    a = 120;
+
+
+    
